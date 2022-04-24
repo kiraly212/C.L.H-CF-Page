aa8347a88971931331a7908447aa4c23e5915953
kiraly212 <92620243+kiraly212@users.noreply.github.com>
2022-04-24 15:31:13 +0900

    Add files via upload

---
+++ js/flickr.js
@@ -0,0 +1,88 @@
+// Flickr API key
+const API_KEY = 'd56709c20967fa2852731e4d217b76d3';
+
+// Flickr画像データのURLを返す
+const getFlickrImageURL = (photo, size) => {
+  let url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${
+    photo.secret
+  }`;
+  if (size) {
+    // サイズ指定ありの場合
+    url += `_${size}`;
+  }
+  url += '.jpg';
+  return url;
+};
+
+// Flickr画像の元ページのURLを返す
+const getFlickrPageURL = (photo) => `https://www.flickr.com/photos/${photo.owner}/${photo.id}`;
+
+// Flickr画像のaltテキストを返す
+const getFlickrText = (photo) => {
+  let text = `"${photo.title}" by ${photo.ownername}`;
+  if (photo.license === '4') {
+    // Creative Commons Attribution（CC BY）ライセンス
+    text += ' / CC BY';
+  }
+  return text;
+};
+
+// リクエストパラメータを作る
+const parameters = $.param({
+  method: 'flickr.photos.search',
+  api_key: API_KEY,
+  tags: 'FF14', // 検索タグ
+  sort: 'interestingness-desc', // ソート論理
+  per_page: 30, // 取得件数
+  license: '0', // All Rights Reserved
+  extras: 'owner_name,license', // 追加で取得する情報
+  format: 'json', // レスポンスをJSON形式に
+  nojsoncallback: 1, // レスポンスの先頭に関数呼び出しを含めない
+});
+const url = `https://api.flickr.com/services/rest/?${parameters}`;
+console.log(url);
+
+// リクエストパラメータの画像を検索して表示
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    // データが取得できなかった場合
+    if (data.stat !== 'ok') {
+      throw new Error('データの取得に失敗しました。');
+    }
+
+    // 空の<div>を作る
+    const $div = $('<div>');
+
+    // ヒット件数
+    $div.append(`<div>${data.photos.total} photos in total</div>`);
+
+    for (let i = 0; i < data.photos.photo.length; i++) {
+      const photo = data.photos.photo[i];
+      const photoText = getFlickrText(photo);
+
+      // $divに <a href="..." ...><img src="..." ...></a> を追加する
+      $div.append(
+        $('<a>', {
+          class: 'd-inline-block img-tooltip',
+          'data-text': photoText,
+          href: getFlickrPageURL(photo),
+          rel: 'noopener noreferrer',
+          target: '_blank', // リンクを新規タブで開く
+        }).append(
+          $('<img>', {
+            src: getFlickrImageURL(photo, 'q'),
+            alt: photoText,
+            width: 300,
+            height: 300,
+          }),
+        ),
+      );
+    }
+    // $divをclass:main-contentsに追加する
+    $div.appendTo('.main-contents');
+  }).catch((error) => {
+    console.error(`エラーが発生しました： ${error.message}`);
+  });
